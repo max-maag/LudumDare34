bb0f34cb8e753766e0c6273a06994097bfc359bc
Max Maag <max-maag@web.de>
2015-12-12 18:55:32 +0100

    Started map generator.

--- Assets/scripts/MapGenerator.cs
+++ Assets/scripts/MapGenerator.cs
@@ -3,15 +3,40 @@ using System.Collections;
 
 public class MapGenerator : MonoBehaviour {
 	private const string PLAYER_TAG = "player";
+
+	public GameObject block;
+	public NormalDistribution positionDistribution;
+	public NormalDistribution heightDistribution;
+	public NormalDistribution gapSizeDistribution;
+
+
 	private GameObject player;
+	private float nextBlockGenPos;
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindWithTag(PLAYER_TAG);
+		nextBlockGenPos = player.transform.position.x;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(player.transform.position.x >= nextBlockGenPos) {
+			float rightEdge = Camera.main.ViewportToWorldPoint(new Vector3(1,0,0)).x;
+
+			Vector3 pos = new Vector3(
+					rightEdge + 1,
+					player.transform.position.y +
+					(float) heightDistribution.NextNormal(),
+					1f);
+			
+			GameObject newBlock = (GameObject) Instantiate(block, pos, Quaternion.identity);
+			newBlock.transform.GetChild(0).localPosition +=
+				new Vector3(0,
+					(float) (3*gapSizeDistribution.dev + gapSizeDistribution.NextNormal()),
+					0);
+			
+			nextBlockGenPos += (float) positionDistribution.NextNormal();
+		}
 	}
 }
---
+++ Assets/scripts/NormalDistribution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class NormalDistribution : MonoBehaviour {
+	private static readonly System.Random seedGenerator = new System.Random();
+	public double mean;
+	public double dev;
+
+	private System.Random random = new System.Random(seedGenerator.Next());
+
+	public double NextNormal() {
+		return mean + dev * Math.Sqrt(-2.0 * Math.Log(random.NextDouble())) *
+			Math.Sin(2.0 * Math.PI * random.NextDouble());
+	}
+}
