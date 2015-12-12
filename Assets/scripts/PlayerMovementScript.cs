f2aa314f41f502c3690f149a3aa8117b73d9aec9
Hong Linh Thai <hong_linh.thai@stud.tu-darmstadt.de>
2015-12-12 18:31:49 +0100

    More Tags and Nudge Code

--- Assets/scripts/PlayerMovementScript.cs
+++ Assets/scripts/PlayerMovementScript.cs
@@ -9,6 +9,8 @@ public class PlayerMovementScript : MonoBehaviour {
 	public float movementSpeed;
 	public float maxSpeed;
 	public float fallingThreshold;
+	public float collisionNudgeThreshold;
+	public float epsilon;
 	private bool dead;
 	public static PlayerMovementScript instance;
 
@@ -30,11 +32,29 @@ public class PlayerMovementScript : MonoBehaviour {
 			Debug.Log("Player fall to much");
 			dead = true;
 		}
+
+		//Debug.Log(this.gameObject.GetComponent<Collider2D>().bounds.min.y);
 	}
 
-	void OnCollisionEnter2D() {
+	void OnCollisionEnter2D(Collision2D c) {
 		if(dead) {
 			Respawn ();
+		}
+
+
+		if(c.gameObject.tag == Tags.BLOCK_TAG) {
+			Bounds boundBlock = c.collider.bounds;
+			Bounds boundPlayer = this.gameObject.GetComponent<Collider2D>().bounds;
+
+			// check if player is below the nudge threshold
+			if ((boundBlock.max.y - boundPlayer.min.y) > boundBlock.extents.y * collisionNudgeThreshold) {
+				Respawn();
+			}
+
+			// player is inside the nudge threshold -> move the player up
+			if((boundBlock.max.y - boundPlayer.min.y) > 0) {
+				this.transform.position = new Vector3(transform.position.x, boundBlock.max.y+boundBlock.extents.y+epsilon, transform.position.z);
+			}
 		}
 	}
 
--- Assets/scripts/Tags.cs
+++ Assets/scripts/Tags.cs
@@ -4,5 +4,6 @@ using System.Collections;
 public class Tags  {
 
 	public const string GROUND_TAG = "ground";
-
+	public const string BLOCK_TAG = "block";
+	public const string PLAYER_TAG = "player";
 }
