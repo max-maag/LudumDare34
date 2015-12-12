39679d0c7e5e3ec1b605c0da6f96d8febf0eba83
Max Maag <max-maag@web.de>
2015-12-12 16:05:18 +0100

    Blocks are destroyed now if they are off screen.

---
+++ Assets/scripts/MapGenerator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGenerator : MonoBehaviour {
+	private const string PLAYER_TAG = "player";
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag(PLAYER_TAG);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
