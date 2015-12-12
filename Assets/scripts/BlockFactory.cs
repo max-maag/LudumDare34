8eec122da0e8ec98470f783a3ac4de447a9bb938
m <michael-rau@gmx.net>
2015-12-12 22:15:28 +0100

    added GroundFactory and (unfinished) TestAnimation scene

---
+++ Assets/scripts/BlockFactory.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockFactory : MonoBehaviour {
+
+	public static BlockFactory instance;
+	private const string SINGLE_BLOCK = "SingleBlock";
+
+	private BlockFactory() {}
+
+	void Start() {
+		instance = this;
+	}
+
+	public GameObject getSingleBlock(float width, float x, float y) {
+		GameObject block = (GameObject) Instantiate(Resources.Load (SINGLE_BLOCK), new Vector3(x, y), Quaternion.identity);
+		block.transform.localScale = new Vector3 (width, 1);
+		return block;
+	}
+
+	// TODO add GroundTunnel
+}
---
+++ Assets/scripts/GroundFactory.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundFactory : MonoBehaviour
+{
+
+	public static GroundFactory instance;
+	private const string EARTH = "GroundEarth";
+
+	private GroundFactory () {}
+
+	void Start () {
+		instance = this;
+	}
+
+	/// <summary>
+	/// Returns an earth ground (bottom half of the screen is ground).
+	/// </summary>
+	/// <returns>The earth.</returns>
+	/// <param name="width">Width.</param>
+	/// <param name="xLeft">The x coordinate destination (left).</param>
+	/// <param name="yTop">The y coordinate destination (top).</param>
+	public GameObject getEarth (float width, float xLeft, float yTop) {
+		GameObject earth = (GameObject) Instantiate (Resources.Load (EARTH), Vector3.zero, Quaternion.identity);
+
+		Vector3 size = earth.GetComponent<Collider2D> ().bounds.size;
+		float y0 = Camera.main.ViewportToWorldPoint (Vector2.zero).y;
+		float desiredWorldHeight = yTop - y0;
+		float yScaleToFitScreen = desiredWorldHeight / size.y;
+		earth.transform.localScale = new Vector3 (width, yScaleToFitScreen);
+		Vector3 sizeAfterScaling = earth.GetComponent<Collider2D> ().bounds.size;
+		earth.transform.position = new Vector3 (sizeAfterScaling.x / 2, y0 + desiredWorldHeight / 2);
+		return earth;
+	}
+
+	// TODO add GroundTunnel
+
+}
