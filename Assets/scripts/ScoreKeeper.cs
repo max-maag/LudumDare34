2d761262e38a79fc5b44b39bdc53a33fea580928
Max Maag <max-maag@web.de>
2015-12-13 17:56:34 +0100

    Prettier fonts.

---
+++ Assets/scripts/ScoreKeeper.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreKeeper : MonoBehaviour {
+	[HideInInspector]
+	public float score;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+	}
+
+	void Update () {
+		score += 100 * Time.deltaTime;
+	}
+}
--- Assets/scripts/UIController.cs
+++ Assets/scripts/UIController.cs
@@ -7,32 +7,20 @@ public class UIController : MonoBehaviour {
 	public CanvasGroup gameOverScreen;
 	public CanvasGroup score;
 
+	private ScoreKeeper playerScore;
+
 	private float startTime;
 
 	// Use this for initialization
 	void Start () {
 		startTime = Time.time;
+		playerScore = gameObject.GetComponent<ScoreKeeper>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		int seconds = (int) Mathf.Floor(Time.time - startTime);
-		int minutes = seconds / 60;
-
-		// Fucking string.Format, how does it work???
-		string time = "";
-
-		if(minutes < 10)
-			time += "0";
-
-		time += minutes + ":";
-
-		if(seconds < 10)
-			time += "0";
-
-		time += seconds;
-
-		score.gameObject.GetComponentInChildren<Text>().text = time;
+		score.gameObject.GetComponentInChildren<Text>().text =
+			"Score: " + (long) Mathf.Round(playerScore.score);
 	}
 
 	void OnPlayerDeath() {
