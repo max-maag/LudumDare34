8ac51ce7d5bf7a2a026c0b2611bbd329567be17a
Hong Linh Thai <hong_linh.thai@stud.tu-darmstadt.de>
2015-12-14 21:40:00 +0100

    Highscore added

--- Assets/scripts/UIController.cs
+++ Assets/scripts/UIController.cs
@@ -5,19 +5,22 @@ using System.Collections;
 
 public class UIController : MonoBehaviour {
 	public CanvasGroup gameOverScreen;
-	public Text score;
-	public Text gameOverScore;
+	public Text scoreTextfield;
+	public Text gameOverScoreTextfield;
+	public Text highScoreTextfield;
 
 	private ScoreKeeper playerScore;
 
 	private float startTime;
 
 	private bool isPlayerDead;
+	private bool scoreCheckDone;
 
 	public float timeTillRestartPossible;
 
 	private const string UP_BUTTON_NAME = "up";
 	private const string DOWN_BUTTON_NAME = "down";
+	private const string HIGHSCORE_NAME = "highscore";
 
 	// Use this for initialization
 	void Start () {
@@ -29,7 +32,7 @@ public class UIController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(!isPlayerDead) {
-			score.text = "Score: " + (long) Mathf.Round(playerScore.score);
+			scoreTextfield.text = "Score: " + (long) Mathf.Round(playerScore.score);
 		} else {
 			if(Input.GetButton(UP_BUTTON_NAME) || Input.GetButton(DOWN_BUTTON_NAME)) {
 				OnRestartButtonClick();
@@ -43,14 +46,37 @@ public class UIController : MonoBehaviour {
 		if(gameOverScreen != null)
 			gameOverScreen.gameObject.SetActive(true);
 
-		if(score != null) {
-			score.gameObject.SetActive(false);
+		if(scoreTextfield != null) {
+			scoreTextfield.gameObject.SetActive(false);
 		}
-		gameOverScore.text = "Score: " + (long) Mathf.Round(playerScore.score);
+
+		float score =  (long) Mathf.Round(playerScore.score);
+
+		gameOverScoreTextfield.text = "Score: " + score;
+
+		if(!scoreCheckDone) {
+			if(checkScore(score)){
+				PlayerPrefs.SetFloat(HIGHSCORE_NAME,score);
+				highScoreTextfield.text = "New highscore: " +PlayerPrefs.GetFloat(HIGHSCORE_NAME) + "!!!";
+			}
+			else {
+				highScoreTextfield.text = "Highscore: " +PlayerPrefs.GetFloat(HIGHSCORE_NAME);
+			}
+			scoreCheckDone = true;
+		}
 	}
 
 	public void OnRestartButtonClick() {
-		if(timeTillRestartPossible <= 0.0f)
+		if(timeTillRestartPossible <= 0.0f) {
 			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+			scoreCheckDone = false;
+		}
+	}
+
+	bool checkScore(float newScore) {
+		if(PlayerPrefs.HasKey(HIGHSCORE_NAME)){
+			return PlayerPrefs.GetFloat(HIGHSCORE_NAME)<newScore;
+		}
+		return true;
 	}
 }
